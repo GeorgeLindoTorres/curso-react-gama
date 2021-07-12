8ac711fe53cd04577b93ef0bef6291ff34a3ef21
George <gwt774@hotmail.com>
2021-07-12 13:50:40 -0300

    organizando a arquitetura dos arquivos

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as S from './styled';
+
+const Repositories = () => {
+    return (
+        <S.Container>
+            <S.Title>Repositories</S.Title>
+            <S.List>
+                <S.ListItem>Repositorio: Nome Repositório</S.ListItem>
+                <S.ListItem>Repositorio: Nome Repositório</S.ListItem>
+                <S.ListItem>Repositorio: Nome Repositório</S.ListItem>
+                <S.ListItem>Repositorio: Nome Repositório</S.ListItem>
+                <S.ListItem>Repositorio: Nome Repositório</S.ListItem>
+                <S.ListItem>Repositorio: Nome Repositório</S.ListItem>
+                <S.ListItem>Repositorio: Nome Repositório</S.ListItem>
+                <S.ListItem>Repositorio: Nome Repositório</S.ListItem>
+                <S.ListItem>Repositorio: Nome Repositório</S.ListItem>
+            </S.List>
+        </S.Container>
+    )
+}
+
+export default Repositories
