d31e0df15f760fe579a69f62dddf1c43d70353fb
George <gwt774@hotmail.com>
2021-07-12 12:43:46 -0300

    Incluindo o react-router-dom e criando a rota de repositories

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
+import React from "react";
+import Routes from "./routes";
 
-
-import { useState } from 'react';
-import axios from 'axios';
-
-function App(props) {
-  const [ usuario, setUsuario ] = useState("")
-  function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
-  }
-
-  return (
-    <>
-      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
-      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
-    </>
+function App() {
+   return (
+   <Routes />
   );
 }
 
